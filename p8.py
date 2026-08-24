8da911de37d63da1d7a58484dc6a60872e167663
aadeshshukla <aadeshshukla470@gmail.com>
2026-08-24 19:35:06 +0530

    winner takes it all

--- p8.py
+++ p8.py
@@ -4,3 +4,14 @@ print("hello world!")
 
 # I'm back 
 # reverse an array 
+def rev_arr(arr):
+    i=0
+    j=len(arr)-1
+    while i<j:
+        arr[i],arr[j]=arr[j],arr[i]
+        i+=1
+        j-=1
+    return arr
+
+lst=[1,2,3,4,5]
+print(rev_arr(lst))
