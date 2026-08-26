0b68e25ca91390feb7a83132db12c313816fdacc
aadeshshukla <aadeshshukla470@gmail.com>
2026-08-26 21:36:19 +0530

    that green

---
+++ p11.py
@@ -0,0 +1,19 @@
+def merge_two_sorted_arrays(arr1,m,arr2,n):
+    i, j, k = m - 1, n - 1, m + n - 1
+    while i >= 0 and j >= 0:
+        if arr1[i] > arr2[j]:
+            arr1[k] = arr1[i]
+            i -= 1
+        else:
+            arr1[k] = arr2[j]
+            j -= 1
+        k -= 1
+    while j >= 0:
+        arr1[k] = arr2[j]
+        j -= 1
+        k -= 1
+
+a=[1, 3, 5, 0, 0, 0]
+b=[2, 4, 6]
+merge_two_sorted_arrays(a, 3, b, 3)
+print(a)
