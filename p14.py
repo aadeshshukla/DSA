d57a64dbbc34ffd2f12139058eb0143efd3a129e
aadeshshukla <aadeshshukla470@gmail.com>
2026-08-31 19:58:21 +0530

    exams sar par hai-2

---
+++ p14.py
@@ -0,0 +1,9 @@
+# DSA Patterns
+# Common patterns used in Data Structures and Algorithms
+# two pointers pattern(slow and fast pointer)
+# sliding window pattern
+# merge intervals pattern
+# hash map pattern
+# binary search pattern
+# dynamic programming pattern
+# recursion pattern
