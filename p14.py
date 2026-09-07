894cee5370e295fade3922d53fd0554661ecd3b6
aadeshshukla <aadeshshukla470@gmail.com>
2026-09-07 15:30:16 +0530

    no more

--- p14.py
+++ p14.py
@@ -132,3 +132,43 @@ def remove_duplicates(nums):
             i+=1
             nums[i]=nums[j]
     return i+1
+
+# test the function
+print(remove_duplicates([1, 1, 2]))  # Output: 2
+
+def container_with_most_water(height):
+    left, right = 0, len(height) - 1
+    max_area = 0
+    while left < right:
+        width = right - left
+        max_area = max(max_area, min(height[left], height[right]) * width)
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_area
+
+# test the function
+print(container_with_most_water([1, 8, 6, 2, 5, 4, 8, 3, 7]))  # Output: 49
+
+
+def trapping_rain_water(height):
+    if not height:
+        return 0
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+    water_trapped = 0
+    while left < right:
+        if left_max < right_max:
+            left += 1
+            left_max = max(left_max, height[left])
+            water_trapped += left_max - height[left]
+        else:
+            right -= 1
+            right_max = max(right_max, height[right])
+            water_trapped += right_max - height[right]
+    return water_trapped
+
+# test the function
+print(trapping_rain_water([0,1,0,2,1,0,1,3,2,1,2,1]))  # Output: 6
+
