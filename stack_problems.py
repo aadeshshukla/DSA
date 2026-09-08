65b2f765519f263fdcad80f3648010807b45db1d
aadeshshukla <aadeshshukla470@gmail.com>
2026-09-08 21:30:30 +0530

    stack

---
+++ stack_problems.py
@@ -0,0 +1,26 @@
+# stack_problems
+# 1) Implement a stack using an array or a linked list.
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop()
+
+    def peek(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+# dsa problems
